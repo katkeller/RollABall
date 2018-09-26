8dd5a69d12e0625fd918020efabfe266b3e69d68
katkeller <43050286+katkeller@users.noreply.github.com>
2018-09-26 00:11:08 -0500

    Added Win Text and Count Text

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     void FixedUpdate()
     {
@@ -26,6 +34,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 
